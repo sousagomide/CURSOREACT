d772df2f08ae830465c439513369855d3b96e572
sousagomide <sousagomide@gmail.com>
2023-02-01 11:24:13 -0300

    Add files via upload

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,15 @@
+// components
+import FirstComponent from './components/FirstComponent';
+// styles/css
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent />
+    </div>
+  );
+}
+
+export default App;
