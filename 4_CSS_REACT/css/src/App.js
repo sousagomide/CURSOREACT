4ec0db8eb4bfbe38d90fc64045b6e8ebe3694f18
sousagomide <sousagomide@gmail.com>
2023-02-07 11:25:01 -0300

    Add files via upload

---
+++ 4_CSS_REACT/css/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import MyComponent from './components/MyComponent';
+
+function App() {
+  const n = 25;
+  const redTitle = true;
+
+  return (
+    <div className="App">
+      <h1>React com CSS</h1>
+      <MyComponent />
+      <p>Este parágrafo é do App.js</p>
+      {/* Inline CSS */}
+      <p style={{color: "#0000FF", padding: "25px", borderTop: "2px solid red"}}>Esse elemento foi estilizado de forma inline</p>
+      {/* CSS Inline dinâmico */}
+      <h2 style={n < 10 ? ({color: "purple"}) : ({color: "pink"})}>CSS dinâmico</h2>
+      {/* CSS Classe dinâmica */}
+      <h2 className={redTitle ? "red-title" : "title"}>Este título possui classe dinâmica</h2>
+    </div>
+  );
+}
+
+export default App;
