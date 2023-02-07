bb4031aa9770b42ba8028544146bb092fca815c3
sousagomide <sousagomide@gmail.com>
2023-02-07 14:05:31 -0300

    Add files via upload

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
+import Title from './components/Title';
 
 function App() {
   const n = 25;
@@ -16,6 +17,9 @@ function App() {
       <h2 style={n < 10 ? ({color: "purple"}) : ({color: "pink"})}>CSS dinâmico</h2>
       {/* CSS Classe dinâmica */}
       <h2 className={redTitle ? "red-title" : "title"}>Este título possui classe dinâmica</h2>
+      {/* CSS Modules */}
+      <Title />
+      <h2 className="my_title">TESTE</h2>
     </div>
   );
 }
